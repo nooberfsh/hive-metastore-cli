f20aea74a848ed870b1a7ae007432229e7f9f4f6
nooberfsh <nooberfsh@gmail.com>
2021-12-16 18:12:41 +0800

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,26 @@ use std::net::ToSocketAddrs;
 
 use thiserror::Error;
 use thrift::protocol::{TBinaryInputProtocol, TBinaryOutputProtocol};
-use thrift::transport::{ReadHalf, TBufferedReadTransport, TBufferedWriteTransport, TIoChannel, TTcpChannel, WriteHalf};
+use thrift::transport::{
+    ReadHalf, TBufferedReadTransport, TBufferedWriteTransport, TIoChannel, TTcpChannel, WriteHalf,
+};
 use thrift::Error as ThriftError;
 
-use hive_metastore::{ThriftHiveMetastoreSyncClient};
+use hive_metastore::ThriftHiveMetastoreSyncClient;
 
 #[allow(unused)]
-pub (crate) mod fb303;
+pub(crate) mod fb303;
 #[allow(unused)]
-pub (crate) mod hive_metastore;
+pub(crate) mod hive_metastore;
 mod models;
-pub use models::*;
 use crate::hive_metastore::{FieldSchema, TThriftHiveMetastoreSyncClient};
-
+pub use models::*;
 
 pub struct HiveMetastoreCli {
-    client: ThriftHiveMetastoreSyncClient<TBinaryInputProtocol<TBufferedReadTransport<ReadHalf<TTcpChannel>>>, TBinaryOutputProtocol<TBufferedWriteTransport<WriteHalf<TTcpChannel>>>>,
+    client: ThriftHiveMetastoreSyncClient<
+        TBinaryInputProtocol<TBufferedReadTransport<ReadHalf<TTcpChannel>>>,
+        TBinaryOutputProtocol<TBufferedWriteTransport<WriteHalf<TTcpChannel>>>,
+    >,
 }
 
 #[derive(Error, Debug)]
@@ -43,7 +47,7 @@ impl HiveMetastoreCli {
 
         // use the input/output protocol to create a Thrift client
         let client = ThriftHiveMetastoreSyncClient::new(i_prot, o_prot);
-        Ok(HiveMetastoreCli{client})
+        Ok(HiveMetastoreCli { client })
     }
 }
 
@@ -53,14 +57,39 @@ impl HiveMetastoreCli {
 impl HiveMetastoreCli {
     pub async fn get_table(&mut self, db: &str, tbl: &str) -> Result<Table> {
         let table = self.client.get_table(db.to_string(), tbl.to_string())?;
-        if !table.view_original_text.unwrap_or_else(||String::new()).trim().is_empty() {
-            return Err(HiveMetastoreError::ViewInsteadOfTable(db.to_string(), tbl.to_string()));
+        if !table
+            .view_original_text
+            .unwrap_or_else(|| String::new())
+            .trim()
+            .is_empty()
+        {
+            return Err(HiveMetastoreError::ViewInsteadOfTable(
+                db.to_string(),
+                tbl.to_string(),
+            ));
         }
         let db_name = table.db_name.unwrap();
         let tbl_name = table.table_name.unwrap();
-        let columns = table.sd.unwrap().cols.unwrap().into_iter().map(|t| t.into()).collect();
-        let partitions = table.partition_keys.unwrap_or_else(|| vec![]).into_iter().map(|t| t.into()).collect();
-        Ok(Table {db_name, tbl_name, columns, partitions})
+        let columns = table
+            .sd
+            .unwrap()
+            .cols
+            .unwrap()
+            .into_iter()
+            .map(|t| t.into())
+            .collect();
+        let partitions = table
+            .partition_keys
+            .unwrap_or_else(|| vec![])
+            .into_iter()
+            .map(|t| t.into())
+            .collect();
+        Ok(Table {
+            db_name,
+            tbl_name,
+            columns,
+            partitions,
+        })
     }
 
     pub async fn get_all_tables(&mut self, db: &str) -> Result<Vec<String>> {
@@ -87,7 +116,7 @@ impl From<FieldSchema> for Column {
         Column {
             name: f.name.unwrap(),
             ty: f.type_.unwrap(),
-            comment: f.comment.unwrap_or_else(||String::new()),
+            comment: f.comment.unwrap_or_else(|| String::new()),
         }
     }
 }
@@ -97,7 +126,7 @@ impl From<FieldSchema> for Partition {
         Partition {
             name: f.name.unwrap(),
             ty: f.type_.unwrap(),
-            comment: f.comment.unwrap_or_else(||String::new()),
+            comment: f.comment.unwrap_or_else(|| String::new()),
         }
     }
 }
