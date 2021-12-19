aef0310aee5544ab1c0b1a1c6996b54b059af656
nooberfsh <nooberfsh@gmail.com>
2021-12-19 16:47:57 +0800

    rest: add apis

--- hive-metastore-rest/src/main.rs
+++ hive-metastore-rest/src/main.rs
@@ -1,5 +1,6 @@
-use axum::{routing::post, response::IntoResponse, Json, Router, AddExtensionLayer};
+use axum::{routing::post, Json, Router, AddExtensionLayer};
 use serde::{Deserialize, Serialize};
+use anyhow::Result;
 
 use hive_metastore_cli::*;
 use axum::extract::Extension;
@@ -7,15 +8,17 @@ use axum::extract::Extension;
 mod config;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    let config = config::parse_config("config.toml").unwrap();
-    let client = HiveMetastoreCli::new(&config.metastore.addr).await.unwrap();
+    let config = config::parse_config("config.toml")?;
+    let client = HiveMetastoreCli::new(&config.metastore.addr).await?;
 
     let app = Router::new()
         .route("/get_table", post(get_table))
+        .route("/get_all_tables", post(get_all_tables))
+        .route("/get_all_databases", post(get_all_databases))
         .layer(AddExtensionLayer::new(client));
 
     // run our app with hyper
@@ -23,22 +26,37 @@ async fn main() {
     tracing::debug!("listening on {}", config.rest.addr);
     axum::Server::bind(&config.rest.addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+    Ok(())
 }
 
-fn to_resp<T: Serialize>(res: Result<T, HiveMetastoreError>) -> impl IntoResponse {
+fn to_resp<T: Serialize>(res: Result<T, HiveMetastoreError>) -> Json<Result<T, String>> {
     let res = res.map_err(|e| e.to_string());
     Json(res)
 }
 
-async fn get_table(Extension(client): Extension<HiveMetastoreCli>, Json(payload): Json<GetTable>) -> impl IntoResponse {
+async fn get_table(Extension(client): Extension<HiveMetastoreCli>, Json(payload): Json<GetTableReq>) -> Json<Result<Table, String>>{
     let res = client.get_table(&payload.db, &payload.tbl).await;
     to_resp(res)
 }
 
+async fn get_all_tables(Extension(client): Extension<HiveMetastoreCli>, Json(payload): Json<GetAllTablesReq>) -> Json<Result<Vec<String>, String>> {
+    let res = client.get_all_tables(&payload.db).await;
+    to_resp(res)
+}
+
+async fn get_all_databases(Extension(client): Extension<HiveMetastoreCli>) -> Json<Result<Vec<String>, String>> {
+    let res = client.get_all_databases().await;
+    to_resp(res)
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct GetTable {
+struct GetTableReq {
     db: String,
     tbl: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct GetAllTablesReq {
+    db: String,
 }
