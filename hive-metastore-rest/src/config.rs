6dd6acb704eaeceae8a3b6317991123adbdf5a6a
nooberfsh <nooberfsh@gmail.com>
2021-12-19 12:36:30 +0800

    rest: first api

--- hive-metastore-rest/src/config.rs
+++ hive-metastore-rest/src/config.rs
@@ -1,23 +1,28 @@
 use std::net::SocketAddr;
 use std::path::Path;
-use std::error::Error;
 
 use anyhow::Context;
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Config {
+    pub rest: Rest,
     pub metastore: Metastore,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Rest {
+    pub addr: SocketAddr,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Metastore {
-    pub host: SocketAddr,
+    pub addr: SocketAddr,
 }
 
 pub fn parse_config<P: AsRef<Path>>(p: P) -> anyhow::Result<Config> {
     let p = p.as_ref();
     let s = std::fs::read(p).with_context(|| format!("failed to read: {}", p.display()))?;
-    let s = toml::from_slice(&s).with_context(||format!("failed to parse to toml"))?;
+    let s = toml::from_slice(&s).with_context(|| format!("failed to parse to toml"))?;
     Ok(s)
 }
--- hive-metastore-rest/src/main.rs
+++ hive-metastore-rest/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::post, response::IntoResponse, Json, Router, AddExtensionLayer};
+use serde::{Deserialize, Serialize};
+
+use hive_metastore_cli::*;
+use axum::extract::Extension;
+
+mod config;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let config = config::parse_config("config.toml").unwrap();
+    let client = HiveMetastoreCli::new(&config.metastore.addr).await.unwrap();
+
+    let app = Router::new()
+        .route("/get_table", post(get_table))
+        .layer(AddExtensionLayer::new(client));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    tracing::debug!("listening on {}", config.rest.addr);
+    axum::Server::bind(&config.rest.addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn to_resp<T: Serialize>(res: Result<T, HiveMetastoreError>) -> impl IntoResponse {
+    let res = res.map_err(|e| e.to_string());
+    Json(res)
+}
+
+async fn get_table(Extension(client): Extension<HiveMetastoreCli>, Json(payload): Json<GetTable>) -> impl IntoResponse {
+    let res = client.get_table(&payload.db, &payload.tbl).await;
+    to_resp(res)
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct GetTable {
+    db: String,
+    tbl: String,
 }
